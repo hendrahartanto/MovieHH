8772a7b0e597c31bbe8264ba32a232b96b95af5c
hendrahartanto <hendrazhuo10@gmail.com>
2025-03-20 16:07:37 +0700

    feat(auth): login & generate refresh access token

--- backend/src/apps/auth/domain/auth.service.ts
+++ backend/src/apps/auth/domain/auth.service.ts
@@ -1,14 +1,19 @@
-import { BadRequestError } from "../../../core/api-error";
+import { AuthFailureError, BadRequestError } from "../../../core/api-error";
 import { SuccessResponse } from "../../../core/api-response";
 import userRepository from "../../user/data-access/user.repository";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import bcrypt from "bcrypt";
+import { LoginUserDTO } from "./dto/login-user.dto";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../../../core/utils/jwt";
 
-export const registerUser = async (registerUserData: CreateUserDTO) => {
+const register = async (registerUserData: CreateUserDTO) => {
   const passwordHash = await bcrypt.hash(registerUserData.password, 10);
 
   const user = await userRepository.getUserByEmail(registerUserData.email);
-  if (user) throw new BadRequestError();
+  if (user) throw new BadRequestError("Email is already taken");
 
   return userRepository.createUser({
     ...registerUserData,
@@ -16,6 +21,20 @@ export const registerUser = async (registerUserData: CreateUserDTO) => {
   });
 };
 
+const login = async (loginUserData: LoginUserDTO) => {
+  const user = await userRepository.getUserByEmail(loginUserData.email);
+  if (!user) throw new BadRequestError("User with given email not found");
+  if (!(await bcrypt.compare(loginUserData.password, user.password))) {
+    throw new AuthFailureError("Invalid Credential");
+  }
+
+  const accessToken = generateAccessToken(user.id);
+  const refreshToken = generateRefreshToken(user.id);
+
+  return { user, accessToken, refreshToken };
+};
+
 export default {
-  registerUser,
+  register,
+  login,
 };
--- backend/src/apps/auth/entry-points/auth.controller.ts
+++ backend/src/apps/auth/entry-points/auth.controller.ts
@@ -3,14 +3,33 @@ import asyncHandler from "../../../middlewares/async-handler";
 import { createUserSchema } from "../domain/dto/create-user.dto";
 import authService from "../domain/auth.service";
 import { SuccessResponse } from "../../../core/api-response";
+import { loginUserSchema } from "../domain/dto/login-user.dto";
 
 const register = asyncHandler(async (req, res) => {
   const validatedData = createUserSchema.parse(req.body);
-  const user = await authService.registerUser(validatedData);
+  const user = await authService.register(validatedData);
 
   new SuccessResponse("Register Successful", { user }).send(res);
 });
 
+const login = asyncHandler(async (req, res) => {
+  const validatedData = loginUserSchema.parse(req.body);
+  const { user, accessToken, refreshToken } = await authService.login(
+    validatedData
+  );
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+    path: "/auth/refresh",
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), //30 days
+  });
+
+  new SuccessResponse("Login Success", { user, token: accessToken }).send(res);
+});
+
 export default {
   register,
+  login,
 };
