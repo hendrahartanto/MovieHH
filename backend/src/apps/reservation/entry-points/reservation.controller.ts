2cd52c7a51f11a8af13cda98ea7df29e754ee13e
hendrahartanto <hendrazhuo10@gmail.com>
2025-03-29 21:06:48 +0700

    fix(reservation): minor syntax error

--- backend/src/apps/reservation/entry-points/reservation.controller.ts
+++ backend/src/apps/reservation/entry-points/reservation.controller.ts
@@ -3,13 +3,13 @@ import asyncHandler from "../../../core/helpers/async-handler";
 import { createReservationSchema } from "../domain/dto/create-reservation.dto";
 import reservationService from "../domain/reservation.service";
 
-const reserve = asyncHandler(async(req, res) => {
+const reserve = asyncHandler(async (req, res) => {
   const validatedData = createReservationSchema.parse(req.body);
   const newReservation = reservationService.reserve(validatedData);
 
-  new SuccessResponse("Reserve succesful", newReservation).send(res):
-})
+  new SuccessResponse("Reserve succesful", newReservation).send(res);
+});
 
-export default{
-  reserve
-}
+export default {
+  reserve,
+};
