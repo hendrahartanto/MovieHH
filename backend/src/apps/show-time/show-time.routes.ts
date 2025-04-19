eea5466897dca7e9c4fadaa105babcd05f5bfea8
hendrahartanto <hendrazhuo10@gmail.com>
2025-04-19 19:06:25 +0700

    feat(authorization): show time & theater authorization

--- backend/src/apps/show-time/show-time.routes.ts
+++ backend/src/apps/show-time/show-time.routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import showTimeController from "./entry-points/show-time.controller";
+import { authenticate, authorize } from "../../middlewares/auth.middleware";
+import { Role } from "@prisma/client";
 
 const router = express.Router();
 
-router.post("/", showTimeController.createShowTime);
+//-------------------------------//
+router.use(authenticate);
+//-------------------------------//
+
+router.post("/", authorize(Role.ADMIN), showTimeController.createShowTime);
 router.get("/", showTimeController.getShowTimeByDate);
 router.get("/:showTimeId/seats", showTimeController.getShowTimeSeats);
 
--- backend/src/apps/theater/theater.routes.ts
+++ backend/src/apps/theater/theater.routes.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import theaterController from "./entry-points/theater.controller";
+import { authenticate, authorize } from "../../middlewares/auth.middleware";
+import { Role } from "@prisma/client";
 
 const router = express.Router();
+
+//-------------------------------//
+router.use(authenticate, authorize(Role.ADMIN));
+//-------------------------------//
 
 router.post("/", theaterController.createTheater);
 
