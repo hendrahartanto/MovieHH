6d29c184af45b3f7d54d993495c9fec3b730da30
hendrahartanto <hendrazhuo10@gmail.com>
2025-05-24 16:41:46 +0700

    feat: authorizatoin middleware

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -5,10 +5,9 @@ import { verifyAccessToken } from "../core/utils/jwt";
 import { ProtectedRequest } from "../types/app-requests";
 import { TokenExpiredError } from "jsonwebtoken";
 import userRepository from "../apps/user/data-access/user.repository";
-import { Role } from "@prisma/client";
 
 export const authenticate = asyncHandler(
-  async (req: ProtectedRequest, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
       const authHeader = req.headers.authorization;
       if (!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -16,30 +15,17 @@ export const authenticate = asyncHandler(
       }
 
       const token = authHeader.split(" ")[1];
+
       const decoded = verifyAccessToken(token) as { userId: string };
       //isi dari decoded ini adalah {userId, iat, exp}
 
       const user = await userRepository.getUserById(decoded.userId);
-      if (!user) throw new AuthFailureError("User not registered!");
-      req.user = user;
 
-      return next();
+      // (req as ProtectedRequest).user = decoded;
+      next();
     } catch (e) {
       if (e instanceof TokenExpiredError) throw new AccessTokenError(e.message);
       throw e;
     }
   }
 );
-
-export const authorize = (role: Role) =>
-  asyncHandler(
-    async (req: ProtectedRequest, res: Response, next: NextFunction) => {
-      if (!req.user || !req.user.role)
-        throw new AuthFailureError("Permission denied");
-
-      const userRole = req.user.role;
-      if (userRole !== role) throw new AuthFailureError("Permission denied");
-
-      return next();
-    }
-  );
