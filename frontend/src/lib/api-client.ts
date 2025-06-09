83011f6637741c5500dc48dd7395cbd416b3f848
hendrahartanto <hendrazhuo10@gmail.com>
2025-06-09 13:35:59 +0700

    feat: handle user not authenticatoin redirect & new raw axios class

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -1,8 +1,12 @@
-import { paths } from "@/config/paths";
 import Axios, { InternalAxiosRequestConfig } from "axios";
 import { getAccessToken, setAccessToken } from "./token-store";
+import { paths } from "@/config/paths";
 
 export const api = Axios.create({
+  baseURL: import.meta.env.VITE_APP_API_URL,
+});
+
+export const rawApi = Axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
 });
 
@@ -14,6 +18,18 @@ const authRequestInterceptor = (config: InternalAxiosRequestConfig) => {
 
   config.withCredentials = true;
   return config;
+};
+
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) prom.reject(error);
+    else prom.resolve(token);
+  });
+
+  failedQueue = [];
 };
 
 const refreshTokenIfNeeded = async (error: any) => {
@@ -33,37 +49,25 @@ const refreshTokenIfNeeded = async (error: any) => {
     isRefreshing = true;
 
     try {
-      const res = await api.get("/auth/refresh");
-      const newAccessToken = res.data.token;
+      const res = await rawApi.get("/auth/refresh", { withCredentials: true });
+      const newAccessToken = res.data.data.token;
       setAccessToken(newAccessToken);
       processQueue(null, newAccessToken);
       originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
       return api(originalRequest);
     } catch (error) {
       processQueue(error, null);
-      //TODO: bisa logout user disini misalya gagal refresh
+
+      const redirectTo = window.location.pathname;
+      window.location.href = paths.auth.login.getHref(redirectTo);
+
       return Promise.reject(error);
     } finally {
       isRefreshing = false;
     }
-  } else if (error.response?.status === 401 && originalRequest._retry) {
-    const redirectTo = window.location.pathname;
-    window.location.href = paths.auth.login.getHref(redirectTo);
   }
 
   return Promise.reject(error);
-};
-
-let isRefreshing = false;
-let failedQueue: any[] = [];
-
-const processQueue = (error: any, token: string | null = null) => {
-  failedQueue.forEach((prom) => {
-    if (error) prom.reject(error);
-    else prom.resolve(token);
-  });
-
-  failedQueue = [];
 };
 
 api.interceptors.request.use(authRequestInterceptor);
--- frontend/src/lib/token-store.ts
+++ frontend/src/lib/token-store.ts
@@ -2,7 +2,6 @@ let accessToken: string | null = null;
 
 export const setAccessToken = (token: string | null) => {
   accessToken = token;
-  console.log("new tokne" + accessToken);
 };
 
 export const getAccessToken = () => accessToken;
