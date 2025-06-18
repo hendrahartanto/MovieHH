75995ab74ea054648473b7f6d1bb6c4e8c732f5f
hendrahartanto <hendrazhuo10@gmail.com>
2025-06-18 11:37:09 +0700

    feat: auth loader spinner

--- frontend/src/app/provider.tsx
+++ frontend/src/app/provider.tsx
@@ -1,4 +1,5 @@
 import { MainErrorFallback } from "@/components/errors/main";
+import { AuthLoader } from "@/lib/auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -11,7 +12,17 @@ export const AppProvider = ({ children }: AppProviderProps) => {
 
   return (
     <ErrorBoundary FallbackComponent={MainErrorFallback}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthLoader
+          renderLoading={() => (
+            <div className="flex items-center justify-center h-screen">
+              <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+            </div>
+          )}
+        >
+          {children}
+        </AuthLoader>
+      </QueryClientProvider>
     </ErrorBoundary>
   );
 };
