c0cf75521e647651286c37695817d1c27f3129d2
hendrahartanto <hendrazhuo10@gmail.com>
2025-07-01 16:46:01 +0700

    feat(genre api): get genre return genre response

--- backend/src/apps/genre/entry-points/genre.controller.ts
+++ backend/src/apps/genre/entry-points/genre.controller.ts
@@ -1,16 +1,13 @@
-import {
-  SuccessMsgResponse,
-  SuccessResponse,
-} from "../../../core/api-response";
+import { SuccessResponse } from "../../../core/api-response";
 import asyncHandler from "../../../core/helpers/async-handler";
 import { createGenreSchema } from "../domain/dto/create-genre.dto";
 import genreService from "../domain/genre.service";
 
 const createGenre = asyncHandler(async (req, res) => {
   const validatedData = createGenreSchema.parse(req.body);
-  await genreService.createGenre(validatedData);
+  const genre = await genreService.createGenre(validatedData);
 
-  new SuccessMsgResponse("Genre Created Successfuly").send(res);
+  new SuccessResponse("Genre Created Successfuly", { genre }).send(res);
 });
 
 const getGenres = asyncHandler(async (req, res) => {
