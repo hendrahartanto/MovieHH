a3fc758beaccf6a05933d51489ff6700da5f0252
hendrahartanto <hendrazhuo10@gmail.com>
2026-02-23 08:04:05 +0700

    feat: adjust upload middleware

--- backend/src/middlewares/upload.middleware.ts
+++ backend/src/middlewares/upload.middleware.ts
@@ -1,9 +1,18 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs"; // 1. Import module fs bawaan Node.js
+
+const uploadDir = "uploads/"; // Definisikan path folder tujuan
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    // 2. Cek apakah folder uploads/ sudah ada
+    if (!fs.existsSync(uploadDir)) {
+      // 3. Jika belum ada, buat foldernya
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
