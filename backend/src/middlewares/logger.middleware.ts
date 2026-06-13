18a5f960b629403c757a40750e03cc23557e9750
hendrahartanto <hendrazhuo10@gmail.com>
2026-06-13 12:17:23 +0700

    fix: avoid logging sensitive request headers

--- backend/src/middlewares/logger.middleware.ts
+++ backend/src/middlewares/logger.middleware.ts
@@ -1,13 +1,36 @@
 import pinoHttp from "pino-http";
 import logger from "../lib/utils/logger";
 
+const DEBUG_HEADER_NAMES = [
+  "content-length",
+  "content-type",
+  "origin",
+  "referer",
+  "user-agent",
+  "x-forwarded-for",
+  "x-forwarded-host",
+  "x-forwarded-proto",
+  "x-request-id",
+];
+
 const loggerMiddleware = pinoHttp({
   logger,
   serializers: {
     req: (req) => ({
       method: req.method,
       url: req.url,
-      headers: req.headers,
+      headers: DEBUG_HEADER_NAMES.reduce<Record<string, string | string[]>>(
+        (headers, headerName) => {
+          const headerValue = req.headers[headerName];
+
+          if (headerValue !== undefined) {
+            headers[headerName] = headerValue;
+          }
+
+          return headers;
+        },
+        {},
+      ),
     }),
     res: (res) => ({
       statusCode: res.statusCode,
